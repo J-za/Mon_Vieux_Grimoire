bee6a2208ad84e4920b26473c3616273051764f0
J-za <jzarrouk@gmail.com>
2025-09-23 12:00:45 +0200

    feat(server): create app.js with Express, Mongoose, CORS setup and main book routes

---
+++ backend/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Book = require('./models/Book');
+
+mongoose.connect('mongodb+srv://jzarrouk:admintest@cluster0.ptjgcg1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.post('/api/books', (req, res, next) => {
+    delete req.body.id;
+    const book = new Book({
+        ...req.body
+    });
+    book.save()
+    .then(() => res.status(201).json({ message : 'Livre enregistré avec succès !' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/books/:id', (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/books', (req, res) => {
+    Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
+});
+
+module.exports = app;
