e465bb2563463cb02907ccef24f86a635ca22cd9
J-za <jzarrouk@gmail.com>
2025-09-24 19:05:24 +0200

    feat(images): implement Multer for image upload on book create, update, and delete

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 require('dotenv').config()
 
 const booksRoutes = require('./routes/books')
@@ -24,5 +25,6 @@ app.use((req, res, next) => {
 
 app.use('/api/books', booksRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
